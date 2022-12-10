7dcd773ec4caf43ed3d244fc82e319c2404dbf41
JW <jonasz.witek@pm.me>
2022-12-10 16:06:18 +0100

    updates

--- components/Home/Title.vue
+++ components/Home/Title.vue
@@ -2,14 +2,36 @@
     <v-parallax height="300"
                 class="mb-4"
                 src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+
         <div class="d-flex flex-column fill-height justify-center align-center text-white">
+            <v-container>
+                <v-row no-gutters>
+                    <v-col class="d-flex justify-center align-center">
+                        <v-sheet class="pa-2 ma-2 bg-transparent">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quod accusamus neque
+                            eveniet. Necessitatibus eum nostrum aliquid perferendis rem neque.``
+                        </v-sheet>
+                    </v-col>
+                    <v-divider vertical></v-divider>
+                    <v-col class="d-flex justify-center align-center">
+                        <v-sheet class="pa-2 ma-2 bg-transparent">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quibusdam maxime dolores?
+                            Esse, dolorem? Aperiam, id voluptas nobis optio debitis, quia facilis vel cum eveniet omnis
+                            eligendi quasi natus ratione velit cumque consequuntur aliquam ea minima qui esse assumenda
+                            possimus voluptatibus earum illo. Omnis, vero. Natus saepe fugit laudantium quis!
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </div>
+        <!-- <div class="d-flex flex-column fill-height justify-center align-center text-white">
             <h1 class="text-h4 font-weight-thin mb-4">
                 Vuetify
             </h1>
             <h4 class="subheading">
                 Build your application today!
             </h4>
-        </div>
+        </div> -->
     </v-parallax>
 </template>
 
