e0659e734f038bd82b619b80bfb71df84ff66f93
JW <jonasz.witek@pm.me>
2022-12-10 15:40:12 +0100

    updates

--- components/Home/Title.vue
+++ components/Home/Title.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-
-    </div>
+    <v-parallax height="300"
+                class="mb-4"
+                src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <div class="d-flex flex-column fill-height justify-center align-center text-white">
+            <h1 class="text-h4 font-weight-thin mb-4">
+                Vuetify
+            </h1>
+            <h4 class="subheading">
+                Build your application today!
+            </h4>
+        </div>
+    </v-parallax>
 </template>
 
 <script setup lang="ts">
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,6 @@
 <template>
     <div>
-        <v-parallax class="ma-2"
-                    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-            <div class="d-flex flex-column fill-height justify-center align-center text-white">
-                <h1 class="text-h4 font-weight-thin mb-4">
-                    Vuetify
-                </h1>
-                <h4 class="subheading">
-                    Build your application today!
-                </h4>
-            </div>
-        </v-parallax>
-
+        <HomeTitle />
 
         <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
             <div class="d-flex flex-column fill-height justify-center align-center text-white">
