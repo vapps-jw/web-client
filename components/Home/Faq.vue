398b9426d886a09db103ff26d10fee974277b564
JW <jonasz.witek@pm.me>
2022-12-17 11:46:46 +0100

    updates

--- components/Home/Faq.vue
+++ components/Home/Faq.vue
@@ -11,15 +11,15 @@
         <v-col class="d-flex justify-center align-center">
             <v-expansion-panels variant="accordion">
                 <v-expansion-panel title="Ile to kosztuje?"
-                                   text="lodsadasdasdassada"></v-expansion-panel>
+                                   text="Wycena oraz wstepna konsultacja są zawsze darmowe. Ostarczny koszt projektu zostaje ustalony po zebraniu wymagań oraz uzgodnieniu szczegółów dotyczących wykonania zadania"></v-expansion-panel>
                 <v-expansion-panel title="Czy wycena jest darmowa?"
                                    text="lodsadasdasdassada"></v-expansion-panel>
                 <v-expansion-panel title="Jak wygląda utrzymanie takiej aplikacji?"
-                                   text="lodsadasdasdassada"></v-expansion-panel>
+                                   text="Oferujemy wsparcie na każdym etapie cyklu życia projektu. Oznacza to że możemy zaoferować pełne wsparcie począwszy od hostingu po nowe funkcjonalności. Nowe funkcjonalności wyceniane są indywidualnie po darmowej konsultacji i zebraniu wymagań."></v-expansion-panel>
                 <v-expansion-panel title="Jakie aplikacje tworzymy?"
-                                   text="lodsadasdasdassada"></v-expansion-panel>
+                                   text="Jesteśmy w stanie stoworzyć szeroki zakres aplikacji od aplikacji desktopowych po webowe oraz mobilne. Chcesz dowiedzieć się czy wykonamy dla Ciebie Twoją wymarzoną aplikację? Napisz do nas"></v-expansion-panel>
                 <v-expansion-panel title="Jak się kontaktować?"
-                                   text="lodsadasdasdassada"></v-expansion-panel>
+                                   text="Preferowaną przez nas formą pierwszego kontaktu jest e-mail. Dostępne dane znajdziesz na stronie 'Kontakt'"></v-expansion-panel>
             </v-expansion-panels>
         </v-col>
     </v-container>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <p>Contact</p>
-    </div>
+    <v-container>
+        <v-sheet class="mx-2 bg-transparent">
+            <p class="text-h4 text-center">VAPPS.PL</p>
+            <p class="text-subtitle-2 text-center">Odkryj co możemy dla Ciebie zrobić</p>
+            <v-divider class="my-4"></v-divider>
+            <p class="text-subtitle-2 text-center">Napisz do Nas</p>
+            <p class="text-h5 text-center">office@vapps.pl</p>
+        </v-sheet>
+    </v-container>
 </template>
 
 <script setup lang="ts">
