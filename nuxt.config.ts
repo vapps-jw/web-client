bd7bab2449f2380f0a3969e80a2ab15edf3c1f39
JW <jonasz.witek@pm.me>
2023-03-05 10:43:29 +0100

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css', '@/assets/main.css'],
-    build: {
-        transpile: ['vuetify']
-    },
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-        head: {
-            title: 'Vapps',
-            link: [
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/favicon.ico'
-                },
-            ]
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+    "@/assets/main.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      title: "Vapps",
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
         },
+      ],
     },
-})
+  },
+});
